9b588709650305a4b1766a434409fc7068ec1f3b
qin185 <2452272793@qq.com>
2025-03-21 12:56:31 +0800

    Initial commit

---
+++ R_project/deck.R
@@ -0,0 +1,75 @@
+die <- c(1, 2, 3, 4, 5, 6)
+die
+is.vector(die)
+type_1 <-c (2, 4, 6, 8)
+type_1
+type_2 <- c(-1L, 2L, 4L)
+type_2
+type_3 <- c("hello", "world")
+type_3
+type_4 <- c(T, F, T, T)
+type_4
+type_5 <- c(1+1i, 1+2i, 2+2i)
+type_5
+raw(3)
+
+names(die) <- c("one", "two", "three", "four", "five", "six")
+attributes(die)
+die
+die+1
+dim(die) <- c(2,3)
+die
+
+m <- matrix(die, nrow = 2)
+m
+m <- matrix(die, nrow = 2, byrow = TRUE)
+m
+
+deck <- data.frame(
+  face = c("king","queen", "jack", "ten", "nine","eight","seven","six",
+           "five","four","three","two","ace","king","queen", "jack", "ten",
+           "nine","eight","seven","six","five","four","three","two","ace",
+           "king","queen", "jack", "ten", "nine","eight","seven","six","five",
+           "four","three","two","ace","king","queen", "jack", "ten", "nine",
+           "eight","seven","six","five","four","three","two","ace"),
+  suit = c("spades","spades","spades", "spades", "spades","spades",
+           "spades","spades", "spades", "spades","spades","spades","spades",
+           "clubs","clubs","clubs","clubs","clubs","clubs","clubs","clubs",
+           "clubs","clubs","clubs","clubs","clubs","diamonds", "diamonds",
+           "diamonds", "diamonds", "diamonds", "diamonds","diamonds","diamonds",
+           "diamonds", "diamonds", "diamonds", "diamonds", "diamonds", "hearts",
+           "hearts", "hearts", "hearts", "hearts", "hearts", "hearts", "hearts",
+           "hearts", "hearts", "hearts", "hearts", "hearts"),
+value = c(13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 13, 12, 11, 10, 9, 8,
+7, 6, 5, 4, 3, 2, 1, 13,12,11,10, 9, 8, 7, 6, 5, 4, 3, 2,1,13,12,11,
+10, 9, 8, 7, 6, 5, 4, 3, 2,1)
+)
+
+faces <- c("king", "queen", "jack", "ten", "nine", "eight", "seven", "six", 
+           "five", "four", "three", "two", "ace")
+suits <- c("spades", "clubs", "diamonds", "hearts")
+values <- c(13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+
+deck <- data.frame(
+  face = rep(faces, 4),
+  suit = rep(suits, each = 13),
+  value = rep(values, 4)
+)
+
+deck[1, ]
+deck[-c(2:nrow(deck)), ]
+deck[1, ]
+deck[1, c("face", "suit", "value")]
+
+deal <- function(deck) {
+  return(deck[1, ])
+}
+# 使用函数
+deal(deck)
+
+shuffle <- function(deck) {
+  shuffled_deck <- deck[sample(nrow(deck)), ]  # 随机打乱行
+  return(shuffled_deck)
+}
+# 使用函数
+shuffle(deck)
---
+++ R_project/roll.R
@@ -0,0 +1,28 @@
+roll <- function(){
+  die <- 1:6
+  dice <-sample(die,size = 2,replace = TRUE)
+  sum(dice)
+}
+rolls <- replicate(10000,roll())
+rolls<- as.data.frame(rolls)
+library(ggplot2)
+ggplot(rolls, aes(x = rolls)) +
+  geom_histogram(binwidth = 1, color = "black", fill = "lightblue") +
+  labs(title = "掷两个骰子 10000 次的结果分布",
+       x = "点数总和",
+       y = "频数")
+
+
+roll3 <- function() {
+  die <- 1:6
+  prob <- c(rep(1/8, 5), 3/8)
+  dice <- sample(die, size = 2, replace = TRUE, prob = prob)
+  sum(dice)
+}
+roll3s <- replicate(10000,roll3())
+roll3s<- as.data.frame(roll3s)
+ggplot(roll3s, aes(x = roll3s)) +
+  geom_histogram(binwidth = 1, color = "black", fill = "lightpink") +
+  labs(title = "掷两个骰子 10000 次的结果分布",
+       x = "点数总和",
+       y = "频数")
